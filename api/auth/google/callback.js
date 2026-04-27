8a8f5b6a92e7e1418172986dfe764f9b4cdf1f36
SantiSmich <santiasbanfield13@gmail.com>
2026-04-27 20:50:11 +0200

    Create callback.js

---
+++ api/auth/google/callback.js
@@ -0,0 +1,25 @@
+export default async function handler(req, res) {
+  const { code } = req.query;
+
+  if (!code) {
+    return res.status(400).send("No code");
+  }
+
+  const params = new URLSearchParams({
+    code,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    client_secret: process.env.GOOGLE_CLIENT_SECRET,
+    redirect_uri: "https://subalert-seven.vercel.app/api/auth/google/callback",
+    grant_type: "authorization_code",
+  });
+
+  const response = await fetch("https://oauth2.googleapis.com/token", {
+    method: "POST",
+    headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    body: params,
+  });
+
+  const data = await response.json();
+
+  res.json(data);
+}
