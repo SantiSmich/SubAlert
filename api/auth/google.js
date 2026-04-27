84e6ddde23681ef0bb5b69287409e96faaf62f42
SantiSmich <santiasbanfield13@gmail.com>
2026-04-27 20:45:24 +0200

    Create google.js

---
+++ api/auth/google.js
@@ -0,0 +1,13 @@
+export default async function handler(req, res) {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+
+  const redirectUri = "https://subalert-seven.vercel.app/api/auth/google/callback";
+
+  const scope = encodeURIComponent(
+    "https://www.googleapis.com/auth/gmail.readonly https://www.googleapis.com/auth/userinfo.email"
+  );
+
+  const url = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}&access_type=offline&prompt=consent`;
+
+  res.redirect(url);
+}
